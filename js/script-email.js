1b9adecc086278dd4e6c2b59b158bce02de42a59
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-02 15:10:42 +0200

    add index-email script

--- js/script-email.js
+++ js/script-email.js
@@ -4,4 +4,35 @@ Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 */
-console.log("here");
+
+//vars
+var usersEmail=["pinco@yahoo.it","panco@gmail.com","cip@icloud.com","ciop@alice.it"];
+//show array on console 
+console.log("Questi sono le email che puoi scegliere: ");
+console.table(usersEmail);
+var userEmail= prompt("inserisci un' email: ","pinco@yahoo.it");
+var isCorrect=false;    //flag 
+//access to html tag
+var access=document.getElementById('access');
+
+//debug inline
+console.log("scelta utente: ",userEmail);
+
+//search for userEmail
+for(i=0;i<usersEmail.length;i++){
+    if(userEmail == usersEmail[i]){
+        isCorrect=true;
+    }
+}
+
+//write html tag response
+if(isCorrect==false){
+    access.innerText=" Spiacente, la sua Email non è inserita.\n Ritenti";
+    access.style.color="#f00";
+}else{
+    access.innerText="La sua email è corretta , può accedere.";
+    access.style.color="#0ca678";
+}
+
+//debug inline
+console.log("valore flag:",isCorrect);
