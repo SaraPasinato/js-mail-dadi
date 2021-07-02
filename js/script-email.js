c48b5fe89abc360476b5f73927bb29b26833f23c
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-02 15:24:37 +0200

    fix comment todo

--- js/script-email.js
+++ js/script-email.js
@@ -13,7 +13,7 @@ console.table(usersEmail);
 var userEmail= prompt("inserisci un' email: ","pinco@yahoo.it");
 var isCorrect=false;    //flag 
 
-/////TODO: validation user input
+
 if (userEmail.length === 0){
     alert("non hai inserito email.");
 }
